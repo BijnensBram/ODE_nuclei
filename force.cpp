cdc94e9272fe9ea7d833728af2974242c339861d
BijnensBram <bram@bijnens.com>
2020-10-31 17:34:51 +0100

    benchmarking

--- force.cpp
+++ force.cpp
@@ -16,7 +16,7 @@ double forcef(double sigmaN, double alphaN, double d, double dref, double cN){
 
 /* Scalar forces between nuclei and cell side or poles*/
 double forceg(double sigma, double M, double alpha, double d, double dref, double c){
-	return sigma*M*pow(d/dref,alpha);
+	return sigma*M*pow(d/dref,alpha)*H(c-d);
 }
 
 /* finite size exclusion */
